1f4055807e3710dd125c2070b86ee2403f72d08a
Alireza Golbad <63424607+Alireza-Golbad@users.noreply.github.com>
2020-12-28 22:55:10 +0330

    first commit

---
+++ scriopt.js
@@ -0,0 +1,34 @@
+"use strict";
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector(".number").textContent = secretNumber;
+document.querySelector(".check").addEventListener("click", function () {
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+    if (!guess) {
+        document.querySelector(".message").textContent = "🚫 No number!!";
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").textContent =
+            "🐱‍🐉 Correct number! 🎉";
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "📈 Too high!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent =
+                "🎃 You lost the game!";
+            document.querySelector(".score").textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1) {
+            document.querySelector(".message").textContent = "🏮 Too low!";
+            score--;
+            document.querySelector(".score").textContent = score;
+        } else {
+            document.querySelector(".message").textContent =
+                "🎃 You lost the game!";
+            document.querySelector(".score").textContent = 0;
+        }
+    }
+});
